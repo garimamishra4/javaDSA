4e5480271c0b9e14ba076f162c6cb47ee303d07d
Garima Mishra <garima26062005@gmail.com>
2025-05-14 22:32:55 +0530

    Create Minmax.java

---
+++ Minmax.java
@@ -0,0 +1,35 @@
+import java.util.*;
+
+public class Minmax
+{
+    public static void main(String[] args)
+    {
+        System.out.println("enter array size:");
+        Scanner v = new Scanner(System.in);
+        int n = v.nextInt();
+        int[] arr = new int[n];
+        System.out.println("enter array elements:");
+        for(int i=0; i<arr.length;i++)
+        {
+        arr[i] = v.nextInt();
+        }
+        int min = arr[0];
+        int max = arr[0];
+        for(int i=0; i<arr.length; i++)
+        {
+           if(arr[i]<min)
+           {
+            min =arr[i];
+           }  
+           if(arr[i]>max)
+           {
+            max = arr[i];
+           }
+        }
+        System.out.println(min);
+        System.out.println(max);
+
+
+
+    }
+}
