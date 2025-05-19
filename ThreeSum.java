c2fee505e8390795a8a03cd41ed5e5df99e89ec8
Garima Mishra <garima26062005@gmail.com>
2025-05-19 23:56:25 +0530

    Update ThreeSum.java

--- ThreeSum.java
+++ ThreeSum.java
@@ -1,1 +1,12 @@
+import java.util.*;
 
+public class ThreeSum
+{
+    public static void main(String[] args)
+    {
+        int[] nums = {-1,0,1,2,-1,-1};
+        int n = nums.length;
+        Arrays.sort(nums);
+        System.out.println(nums);
+    }
+}
