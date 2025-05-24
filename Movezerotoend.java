e58a46b20e53aca8d218b8a008d2e9d1edec36ac
Garima Mishra <garima26062005@gmail.com>
2025-05-24 22:53:07 +0530

    Update Movezerotoend.java

--- Movezerotoend.java
+++ Movezerotoend.java
@@ -1,17 +1,14 @@
  // move all zeros from given array to it's end while maintaing the relative order.
- import java.util.*;
-
- public class Movezerotoend
- {
-    static void pushZeros(int[] arr)
-    {
-        int n = arr.length;
+//leetcode solution
+ class Movezerotoend {
+    public void moveZeroes(int[] nums) {
+         int n = nums.length;
         int[] temp = new int[n];
         int j = 0;
         for(int i=0; i<n; i++)
         {
-            if(arr[i]!=0)
-            temp[j++] = arr[i];
+            if(nums[i]!=0)
+            temp[j++] = nums[i];
         }
         while(j<n)
         {
@@ -19,17 +16,8 @@
         }
         for(int i=0; i<n;i++)
         {
-            arr[i] = temp[i];
+            nums[i] = temp[i];
         }
     }
-
-    public static void main(String[] args)
-    {
-        int[] arr = {1,2,0,4,3,0,5,0};
-        pushZeros(arr);
-        for(int num : arr)
-        {
-            System.out.println(num + " ");
-        }
-    }
- } 
+}
+   
